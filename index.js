2fe3c4ebcc2159513b09219f606629586bc9a5ec
Ronney Arias <ronneyarias@gmail.com>
2023-10-10 23:51:37 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(desiredCity) {
+  return desiredCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipLevel) {
+  if (tipLevel === 'generous') {
+    return 'Thank you so much.';
+  } else if (tipLevel === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
+  
 }
